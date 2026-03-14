0cef0c347ce5dde2541965c32e25e5247337e73b
Rajeshkarwasara <rajeshjaat280@gmail.com>
2026-03-14 16:22:02 +0530

    port1

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@
   const headerToggleBtn = document.querySelector('.header-toggle');
 
   function headerToggle() {
-    document.querySelector('#header').classList.toggle('header-show');
+    // document.querySelector('#header').classList.toggle('header-show');
     headerToggleBtn.classList.toggle('bi-list');
     headerToggleBtn.classList.toggle('bi-x');
   }
